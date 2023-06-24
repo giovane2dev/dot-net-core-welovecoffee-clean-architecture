b0cbedd8904c0b83da19c5f58fbe47a08d267289
Two Dev <contato.twodev@gmail.com>
2023-06-24 16:11:25 -0300

    refactor: Adds Comments For Best Comunication

--- PromoBeta.Domain/Entities/Category.cs
+++ PromoBeta.Domain/Entities/Category.cs
@@ -5,6 +5,11 @@ namespace PromoBeta.Domain.Entities
 {
     public sealed class Category : Base
     {
+        // from anemic model for rich model
+
+        // applies the single responsibility principle enriching the class and setting
+        // specific attributes, validanting your properties in construnction.
+
         public string Name { get; private set; }
 
         public Category(string name)
--- PromoBeta.Domain/Entities/Product.cs
+++ PromoBeta.Domain/Entities/Product.cs
@@ -3,11 +3,18 @@
 namespace PromoBeta.Domain.Entities
 {
     public sealed class Product : Base
-    {        
+    {
+        // from anemic model for rich model
+
+        // applies the single responsibility principle enriching the class and setting
+        // specific attributes, validanting your properties in construnction.
+
         public string Name { get; private set; }
         public string Description { get; private set; }
         public decimal Price { get; private set; }
         public string Image { get; private set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
 
         public Product(string name, string description, decimal price, string image)
         {
@@ -29,15 +36,12 @@ namespace PromoBeta.Domain.Entities
             this.ValidateDomain(name, description, price, image);
         }
 
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
-
         private void ValidateDomain(string name, string description, decimal price, string image)
         {
             DomainExceptionValidation.Check(string.IsNullOrEmpty(name),
                 "Informe o Nome do Produto!");
 
-            DomainExceptionValidation.Check(price < 0,
+            DomainExceptionValidation.Check(price == 0,
                 "Informe o Valor do Produto!");
 
             Name = name;
