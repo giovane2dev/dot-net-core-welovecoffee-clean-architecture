86f7ddec4da42b163c8603bbbcad391d8a13f5d7
Two Dev <contato.twodev@gmail.com>
2023-07-04 17:56:45 -0300

    feat: Adds Migration Files For Use With Entity Framework
    - updates database for insert sample data

---
+++ PromoBeta.Infra.Data/Migrations/20230704204335_SampleProducts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PromoBeta.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SampleProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Image,CategoryId)" +
+                "VALUES('Produto 01','Produto 01',10.5,null,1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Image,CategoryId)" +
+                "VALUES('Produto 02','Produto 02',12.5,null,1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
