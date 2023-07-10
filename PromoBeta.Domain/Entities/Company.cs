b50ba6d677278f31c01796020e252060183fdd76
Two Dev <contato.twodev@gmail.com>
2023-07-10 17:54:09 -0300

    feat: Creates A New 'POCO' Class As Domain Entity
    - enriches the anemic class setting its attributes while construction
    - validates properties

---
+++ PromoBeta.Domain/Entities/Company.cs
@@ -0,0 +1,53 @@
+﻿using PromoBeta.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoBeta.Domain.Entities
+{
+    public sealed class Company : Base
+    {
+        // from anemic model for rich model
+
+        // applies the single responsibility principle enriching the class and setting
+        // specific attributes, validanting your properties in construnction.
+
+        // equivalent to CNPJ in Brazil (EIN)
+        public string TaxIdNumber { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+        public Company(string taxIdNumber, string name, string description)
+        {
+            this.Validate(taxIdNumber, name, description);
+        }
+
+        public Company(int id, string taxIdNumber, string name, string description)
+        {
+            DomainExceptionValidation.Check(id <= 0,
+                "Código da Empresa inválido!");
+
+            Id = id;
+
+            this.Validate(taxIdNumber, name, description);
+        }
+
+        public void Validate(string taxIdNumber, string name, string description)
+        {
+            DomainExceptionValidation.Check(string.IsNullOrEmpty(taxIdNumber),
+               "Informe o CNPJ da Empresa!");
+
+            DomainExceptionValidation.Check(string.IsNullOrEmpty(name),
+                "Informe o Nome da Empresa!");
+
+            TaxIdNumber = taxIdNumber;
+            Name = name;
+            Description = description;
+            Active = true;
+        }
+    }
+}
