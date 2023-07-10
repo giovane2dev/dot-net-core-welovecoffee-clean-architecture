f96f7d93a157135a8bbdd52eb7e40676b17e5bd6
Two Dev <contato.twodev@gmail.com>
2023-07-10 18:06:29 -0300

    refactor: Reorganizes Code
    - removes unused namespaces

--- PromoBeta.Domain/Entities/Company.cs
+++ PromoBeta.Domain/Entities/Company.cs
@@ -1,9 +1,5 @@
-﻿using PromoBeta.Domain.Validation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using PromoBeta.Domain.Validation;
 
 namespace PromoBeta.Domain.Entities
 {
