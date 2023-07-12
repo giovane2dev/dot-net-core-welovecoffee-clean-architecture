03ea4a5a3190e61c93dd1daa58e8d61912a91b03
Two Dev <contato.twodev@gmail.com>
2023-07-12 18:08:49 -0300

    feat: Creates A New Unit Test For Companies
    - adds methods for test with and without valid parameters

---
+++ PromoBeta.Domain.Tests/CompanyUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using PromoBeta.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PromoBeta.Domain.Tests
+{
+    public class CompanyUnitTest
+    {
+        [Fact(DisplayName = "create a first company with valid parameters")]
+        public void CreateCompany_WithValidParameters()
+        {
+            Action action = () => new Company(id:1, taxIdNumber:"8983", name:"DEXS", description:"");
+
+            action.Should().NotThrow<PromoBeta.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "creates a first company with invalid parameters")]
+        public void CreateCompany_WithoutValidParameters()
+        {
+            Action action = () => new Company(id: 1, taxIdNumber: "8983", name: String.Empty, description: "");
+
+            action.Should().NotThrow<PromoBeta.Domain.Validation.DomainExceptionValidation>();
+        }
+    }
+}
