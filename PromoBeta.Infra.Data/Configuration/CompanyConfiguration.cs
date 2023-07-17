c295d4e405df0352e5ebd9ea6b3e32510d94d71a
Two Dev <contato.twodev@gmail.com>
2023-07-17 15:11:33 -0300

    refactor: Adds New Entitity Configuration For The Company POCO Class

---
+++ PromoBeta.Infra.Data/Configuration/CompanyConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PromoBeta.Domain.Entities;
+
+namespace PromoBeta.Infra.Data.Configuration
+{
+    internal class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.TaxIdNumber).HasMaxLength(15).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(150).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(250);
+        }
+    }
+}
