e0081f0b5630b23c0d8c9852cb4825e49d9a1ebf
Two Dev <contato.twodev@gmail.com>
2023-07-17 16:32:47 -0300

    feat: Creates Samples For The Migration
    - adds samples for companies
    - adds samples for categories
    - adds samples for products

---
+++ PromoBeta.Infra.Data/Migrations/20230717185450_SampleCompany.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PromoBeta.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SampleCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /*
+            migrationBuilder.Sql("INSERT INTO Companies (TaxIdNumber,Name,Description,Active)" +
+                "VALUES ('50946701000196','DEXS','DEXS SOLUTIONS TECNOLOGIA E INTERNET',1)");
+            */
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // migrationBuilder.Sql("DELETE FROM Companies");
+        }
+    }
+}
