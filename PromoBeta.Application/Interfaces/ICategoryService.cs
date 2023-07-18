54903fedf10cd41e99111c2e22f607c6ef89864a
Two Dev <contato.twodev@gmail.com>
2023-07-18 17:38:22 -0300

    refactor: Reorganizes Signatures

--- PromoBeta.Application/Interfaces/ICategoryService.cs
+++ PromoBeta.Application/Interfaces/ICategoryService.cs
@@ -7,10 +7,8 @@ namespace PromoBeta.Application.Interfaces
     public interface ICategoryService
     {
         Task CreateAsync(CategoryDTO category);
-
         Task<IEnumerable<CategoryDTO>> GetCategoriesAsync();
         Task<CategoryDTO> GetByIdAsync(int? id);
-
         Task UpdateAsync(CategoryDTO category);
         Task DeleteAsync(CategoryDTO category);
     }
--- PromoBeta.Application/Interfaces/IProductService.cs
+++ PromoBeta.Application/Interfaces/IProductService.cs
@@ -7,10 +7,8 @@ namespace PromoBeta.Application.Interfaces
     public interface IProductService
     {
         Task CreateAsync(ProductDTO product);
-
         Task<IEnumerable<ProductDTO>> GetProductsAsync();
         Task<ProductDTO> GetByIdAsync(int? id);
-
         Task UpdateAsync(ProductDTO product);
         Task DeleteAsync(ProductDTO product);
     }
