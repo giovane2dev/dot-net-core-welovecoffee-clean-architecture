8ce165c5708d62f9701b5413e0ef0e05bd678a0c
Two Dev <contato.twodev@gmail.com>
2023-07-18 17:39:13 -0300

    feat: Creates New Services Class For Company

---
+++ PromoBeta.Application/Interfaces/ICompanyServices.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PromoBeta.Application.DTOs;
+
+namespace PromoBeta.Application.Interfaces
+{
+    public interface ICompanyServices
+    {
+        Task CreateAsync(CompanyDTO company);
+        Task<IEnumerable<CompanyDTO>> GetCompaniesAsync();
+        Task<CompanyDTO> GetByIdAsync(int? id);
+        Task UpdateAsync(CompanyDTO company);
+        Task DeleteAsync(CompanyDTO company);            
+    }
+}
