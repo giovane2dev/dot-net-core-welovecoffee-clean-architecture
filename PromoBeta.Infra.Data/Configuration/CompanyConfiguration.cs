c633d5aaba38339b74782f3c9ec1163bfc073259
Two Dev <contato.twodev@gmail.com>
2023-07-18 17:34:32 -0300

    refactor: Updates Max Lenght For The 'TaxIdNumber" Field For '15'

--- PromoBeta.Infra.Data/Configuration/CompanyConfiguration.cs
+++ PromoBeta.Infra.Data/Configuration/CompanyConfiguration.cs
@@ -9,7 +9,7 @@ namespace PromoBeta.Infra.Data.Configuration
         public void Configure(EntityTypeBuilder<Company> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.TaxIdNumber).HasMaxLength(25).IsRequired();
+            builder.Property(x => x.TaxIdNumber).HasMaxLength(15).IsRequired();
             builder.Property(x => x.Name).HasMaxLength(150).IsRequired();
             builder.Property(x => x.Description).HasMaxLength(250);
         }
